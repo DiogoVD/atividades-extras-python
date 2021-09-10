1b50a177c9417e879960906cb644f32df253ff08
Diogo <diogovdamascena@gmail.com>
2021-09-10 20:16:19 -0300

    criando interface em python

---
+++ interface grafica py/tela1.py
@@ -0,0 +1,45 @@
+import PySimpleGUI as sg
+
+class TelaPy:
+    def __init__(self):
+        sg.change_look_and_feel('DarkBlue14')
+        #layout
+        layout = [
+            [sg.Text('Nome',size=(5,0)),sg.Input(size=(15,0),key='nome')],
+            [sg.Text('Idade',size=(5,0)),sg.Input(size=(15,0),key='idade')],
+            [sg.Text('Quais provedores de e-mail são aceitos?')],
+            [sg.Checkbox('Gmail',key='gmail'),sg.Checkbox('Outlook',key='outlook'),sg.Checkbox('Yahoo',key='yahoo')],
+            [sg.Text('Aceita cartão')],
+            [sg.Radio('Sim','cartoes',key='aceita_cartao'),sg.Radio('Não','cartoes',key='naoAceita_cartao')],
+            [sg.Slider(range=(0,255),default_value=0,orientation='h',size=(15,15),key='sliderVelocity')],
+            [sg.Button('Enviar Dados')],
+            [sg.Output(size=(30,15))]
+        ]
+        #janela
+        self.janela = sg.Window('Dados do Usuário').layout(layout)
+
+
+    def Iniciar(self):
+        while True:
+            #Extrair dados da tela
+            self.button, self.values = self.janela.Read()
+            nome = self.values['nome']
+            idade = self.values['idade']
+            aceita_gmail = self.values['gmail']
+            aceita_outlook = self.values['outlook']
+            aceita_yahoo = self.values['yahoo']
+            aceita_cartao = self.values['aceita_cartao']
+            Nao_aceita_cartao = self.values['naoAceita_cartao']
+            sliderVelocity = self.values['sliderVelocity']
+
+            print(f'nome: {nome}')
+            print(f'idade: {idade}')
+            print(f'aceita_gmail: {aceita_gmail}')
+            print(f'aceita_outlook: {aceita_outlook}')
+            print(f'aceita_yahoo: {aceita_yahoo}')
+            print(f'aceita_cartao: {aceita_cartao}')
+            print(f'naoAceita_cartao: {Nao_aceita_cartao}')
+            print(f'sliderVelocity: {sliderVelocity}')
+
+tela = TelaPy()
+tela.Iniciar()
